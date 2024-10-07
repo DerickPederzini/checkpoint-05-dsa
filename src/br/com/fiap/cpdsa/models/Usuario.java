2cc12a0c070b96ae1c395ebbc827ed26ddee6a5d
Marcelo Furlanetto <marcelofurla0813@gmail.com>
2024-10-07 08:32:56 -0300

    created

---
+++ src/br/com/fiap/cpdsa/models/Usuario.java
@@ -0,0 +1,72 @@
+package br.com.fiap.cpdsa.models;
+
+public class Usuario implements Comparable<Usuario>{
+    private String nome;
+    private String cpf;
+    private String whatsapp;
+    private double totalCompras;
+    private boolean aptoOferta;
+
+    public Usuario(String nome, String cpf, String whatsapp, double totalCompras, boolean aptoOferta) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.whatsapp = whatsapp;
+        this.totalCompras = totalCompras;
+        this.aptoOferta = aptoOferta;
+    }
+
+    public Usuario() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getWhatsapp() {
+        return whatsapp;
+    }
+
+    public void setWhatsapp(String whatsapp) {
+        this.whatsapp = whatsapp;
+    }
+
+    public double getTotalCompras() {
+        return totalCompras;
+    }
+
+    public void setTotalCompras(double totalCompras) {
+        this.totalCompras = totalCompras;
+    }
+
+    public boolean isAptoOferta() {
+        return aptoOferta;
+    }
+
+    public void setAptoOferta(boolean aptoOferta) {
+        this.aptoOferta = aptoOferta;
+    }
+
+    @Override
+    public int compareTo(Usuario o) {
+        if(this.cpf.compareTo(o.getCpf()) == 1) {
+            return 1;
+        }
+        if(this.cpf.compareTo(o.getCpf()) == -1) {
+            return -1;
+        }
+        return 0;
+    }
+}
